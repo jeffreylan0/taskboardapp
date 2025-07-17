8812c203b7ff0972850174064220a483ba64927e
jeffreylan0 <127343526+jeffreylan0@users.noreply.github.com>
2025-07-16 20:42:10 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,13 @@
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
-import { Toaster } from 'sonner'; // Import from sonner
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      {/* The ThemeProvider handles light/dark mode switching. */}
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <Component {...pageProps} />
-        {/* The Toaster from sonner uses the page's theme automatically */}
-        <Toaster richColors />
       </ThemeProvider>
     </SessionProvider>
   );
